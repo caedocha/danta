b81b39c9883c9ef9037a588b422335fb6f176528
Carlos Chacon <caedo.00@gmail.com>
2016-12-09 17:30:54 -0600

    Deprecate http endpoint

--- danta_api.rb
+++ danta_api.rb
@@ -13,18 +13,6 @@ class DantaAPI < Sinatra::Base
     RequestHandler.new(request: request).process
   end
 
-  get '/videos' do
-    VideoLibrary.videos.to_json
-  end
-
-  get '/launch' do
-    Pecari::Player.launch(params[:video])
-  end
-
-  get '/exec' do
-    Pecari::Player.send(params[:command])
-  end
-
   run! if __FILE__ == $0
 
 end
